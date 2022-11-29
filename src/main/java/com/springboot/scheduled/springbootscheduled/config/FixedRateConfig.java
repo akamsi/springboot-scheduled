d32a4f135f9ace23c96fb88a8d65897336fe63eb
akamsi <sax.747@gmail.com>
2022-11-29 16:59:30 +0200

    Add fixed rate example

--- src/main/java/com/springboot/scheduled/springbootscheduled/config/FixedRateConfig.java
+++ src/main/java/com/springboot/scheduled/springbootscheduled/config/FixedRateConfig.java
@@ -1,2 +1,20 @@
-package com.springboot.scheduled.springbootscheduled.config;public class FixedRateConfig {
+package com.springboot.scheduled.springbootscheduled.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Configuration
+@EnableScheduling
+public class FixedRateConfig {
+
+    @Scheduled(fixedRate = 1000)
+    public void scheduleFixedRateTask() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
+        System.out.println("[Fixed Rate] - Task starts now at " + dtf.format(LocalDateTime.now()));
+    }
+
 }
