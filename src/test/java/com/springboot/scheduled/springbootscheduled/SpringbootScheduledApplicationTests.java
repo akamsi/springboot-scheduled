1b4d9cf38d1350428787ea2146dffa77dd17b629
akamsi <sax.747@gmail.com>
2022-11-29 16:18:03 +0200

    Initial commit

---
+++ src/test/java/com/springboot/scheduled/springbootscheduled/SpringbootScheduledApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.scheduled.springbootscheduled;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootScheduledApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
